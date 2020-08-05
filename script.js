0206b7bea594206e86ef899c7dc6813013cd1fbe
ironheartbj18 <ironheartbj18@gmail.com>
2020-08-05 01:23:26 -0500

    move left and right

--- script.js
+++ script.js
@@ -3,15 +3,18 @@ document.addEventListener('DOMContentLoaded',()=>{
     let squares = document.querySelectorAll('.grid div');
     const displayScore = document.querySelector('#score');
     const startGame = document.querySelector('#start-game');
-    const width = 10;// ??????
+    const width = 10;
     let timerId;
+    let nextRandom = 0;
 
+
+    //==============================================================================================================================================
     const designTetrominoes = [
         [0,0],[0,1],[0,2],
         [1,0],[1,1],[1,2],
         [2,0],[2,1],[2,2]
     ]
-// build Tetrominoes
+    // build Tetrominoes
     const lTetrominoesCustom = [
         [designTetrominoes[1],designTetrominoes[4],designTetrominoes[7],designTetrominoes[8]],
         [designTetrominoes[2],designTetrominoes[3],designTetrominoes[4],designTetrominoes[5]],
@@ -22,12 +25,10 @@ document.addEventListener('DOMContentLoaded',()=>{
     const wholeTetrominoes = lTetrominoesCustom[0][0].concat(lTetrominoesCustom[0][1],lTetrominoesCustom[0][2],lTetrominoesCustom[0][3]);
     // console.log(wholeTetrominoes);
     const here = wholeTetrominoes.join('')
-    console.log(here);
+    console.log("want to add two digital and comma: " + here);
+    // need to figuare it out how to covert into shape Tetromino [01, 11, 21, 02] probably RegExp???
 
-
-    // need to figuare it out how to covert into shape Tetromino [1, 11, 21, 2] probably RegExp???
-
-    //=======================================================================
+    //==============================================================================================================================================
 
     const lTetromino = [
         [1, width+1, width*2+1, 2],
@@ -60,8 +61,6 @@ document.addEventListener('DOMContentLoaded',()=>{
         [width,width+1,width+2,width+3]
     ]
     const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
-    console.log(theTetrominoes[0]);
-    console.log(theTetrominoes);
     let random = Math.floor(Math.random()*theTetrominoes.length);
     let currentPosition = 4;
     let currentRotation = 0;
@@ -70,20 +69,20 @@ document.addEventListener('DOMContentLoaded',()=>{
     // draw the first rotation in the first tetromino
     function draw(){
         current.forEach(index => {
-        squares[currentPosition + index ].classList.add('tetromino');
+        squares[currentPosition + index].classList.add('tetromino');
       });
     }
+    // draw();
 
     // undraw tetromino shape
     function undraw(){
       current.forEach(index =>{
         squares[currentPosition + index].classList.remove('tetromino')  
-        // squares[index].style.backgroundColor = '';  
+        // squares[index].style.backgroundColor = '';  // dont need this one due to remove() method
     });
     }
 
     /*
-
         if(keycode === 'down') {
             call function as loop
         if (squares[index].matches('tetromino')) {
@@ -93,70 +92,73 @@ document.addEventListener('DOMContentLoaded',()=>{
         }
         }
     */   
-    // newDraw create new tetromino shape
-    function newDraw(){
-        current.map(index =>{
-            squares[currentPosition + index+ 10].classList.add('tetromino');
-            console.log(squares); 
-            //squares += [index+10];
-        })
-    }    
+    // newDraw create new tetromino shape    
     
-
-
-    
-    
+    // timerId = setInterval(moveDownAuto, 250); 
+window.addEventListener('keyup', control);
     window.addEventListener('keydown', (e) => {
         if(e.keyCode === 13){// Enter to start game
             draw();
-            timerId = setInterval(moveDownAuto, 500);
+            timerId = setInterval(moveDownAuto, 250);
+            
         }
 
-        //match key code all the arrows. 
+        //match key code all the arrows. for rotote 
 
     })    
     
     function moveDownAuto(){
-    undraw();
-    currentPosition += 10;
-    newDraw();
+        undraw();
+        currentPosition += width;
+        draw();
+        freeze();
     }
-moveDownAuto(); // motionable the tetromino game.
+    
 
+    // freeze function
+    function freeze(){
+        if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))){
+            current.forEach(index => squares[currentPosition + index].classList.add('taken'));
+            
+            nextRandom = Math.floor(Math.random() * theTetrominoes.length);
+            current = theTetrominoes[nextRandom][currentRotation];
+            currentPosition = 4; // reset grid number
+        }
+        draw();
+    }
 
+    function moveLeft(){
+      undraw();
+      const isAtLeftEdge = current.some(index=> (currentPosition + index) % width === 0); // remainder width
+      if(!isAtLeftEdge) currentPosition -=1;
+      if(current.some(index=> squares[currentPosition + index].classList.contains('taken'))){
+        currentPosition +=1;
+      }
+      draw();
+    }
 
-    // clearInterval(timerId);
-    // draw();
+    function moveRight(){
+      undraw();
+      const isAtRightEdge = current.some(index => (currentPosition + index) % width === width -1);
+      if(!isAtRightEdge) currentPosition +=1;
+      if(current.some(index=> squares[currentPosition + index].classList.contains('taken'))){
+          currentPosition -=1;
+      }
+      draw();
+    }
+
+    //assign functions to keyCodes
+    function control(e){
+      if(e.keyCode === 37){
+          moveLeft();
+      }
+      else if(e.keyCode === 39){
+          moveRight();
+      }
+    }
+
     
 
 
-     
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-     
-    function controls(e){
-      window.keyCode;
-      console.log(e);
-    }
-
-    window.addEventListener('keydown', event=>{
-        console.log(event.keyCode);
-    })
-
-
 } )
+// 55:52
